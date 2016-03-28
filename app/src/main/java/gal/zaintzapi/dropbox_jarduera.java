7b8f5f38f57184c24dcda4f9a16bdeab99032cf3
aechezarraga003 <aechezarraga003@ikasle.ehu.eus>
2016-03-28 22:55:32 +0200

    Dropbox oauth

--- app/src/main/java/gal/zaintzapi/dropbox_jarduera.java
+++ app/src/main/java/gal/zaintzapi/dropbox_jarduera.java
@@ -34,7 +34,7 @@ public class dropbox_jarduera extends Activity {
     final static public String DROPBOX_APP_SECRET = "4v8lv8bv5cm9iqp";
 
     final static public Session.AccessType ACCESS_TYPE = Session.AccessType.DROPBOX;
-    private DropboxAPI mApi;
+    private DropboxAPI<AndroidAuthSession> mApi;
     private Button bttn_ver_info;
 
 
@@ -84,77 +84,31 @@ public class dropbox_jarduera extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dropbox_jarduera);
-        AndroidAuthSession session = buildSession();
-        mApi = new DropboxAPI(session);
-       /* mApi.getFileStream('\\','hola');*/
+        AppKeyPair appKeys = new AppKeyPair(DROPBOX_APP_KEY, DROPBOX_APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mApi = new DropboxAPI<AndroidAuthSession>(session);
 
         bttn_ver_info=(Button)findViewById(R.id.bttnDrop_box_ver_info_cuenta);
         bttn_ver_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                llamada_ws l = new llamada_ws(dropbox_jarduera.this, mApi);
-                try {
-                    Thread.sleep(90);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-                if (l.isCorrecta()) {
-                    if (!l.getResultado().toString().equals("false")) {
-                       Object obj = l.getResultado();
-                        int a=0;
-
-                    }
-                }
+                mApi.getSession().startOAuth2Authentication(dropbox_jarduera.this);
             }
         });
     }
 
+    protected void onResume() {
+        super.onResume();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (mApi.getSession().authenticationSuccessful()) {
+            try {
+                // Required to complete auth, sets the access token on the session
+                mApi.getSession().finishAuthentication();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    private String[] getKeys() {
-        String key = DROPBOX_APP_KEY;
-        String secret = DROPBOX_APP_SECRET;
-        if (key != null && secret != null) {
-            String[] ret = new String[2];
-            ret[0] = key;
-            ret[1] = secret;
-            return ret;
-        } else {
-            return null;
+                String accessToken = mApi.getSession().getOAuth2AccessToken();
+            } catch (IllegalStateException e) {
+                Log.i("DbAuthLog", "Error authenticating", e);
+            }
         }
     }
-
-
-    private AndroidAuthSession buildSession() {
-        AppKeyPair appKeyPair = new AppKeyPair(DROPBOX_APP_KEY,DROPBOX_APP_SECRET);
-        AndroidAuthSession session;
-
-        String[] stored = getKeys();
-        if (stored != null) {
-            AccessTokenPair accessToken = new AccessTokenPair(stored[0],
-                    stored[1]);
-            session = new AndroidAuthSession(appKeyPair,ACCESS_TYPE,
-                    accessToken);
-        } else {
-            session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
-        }
-
-        return session;
-    }
-
 }
